271e37fbf1b70cbfeb68aba77c08adf682954d77
BenjaminKoskas <benjaminkoskas9@gmail.com>
2021-11-11 23:43:54 +0100

    [ADD] premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "GLbox"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GLbox"
+	location "GLbox"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GLB_PLATFORM_WINDOWS",
+			"GLB_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GLB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GLB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GLB_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GLbox/vendor/spdlog/include",
+		"GLbox/src"
+	}
+
+	links
+	{
+		"GLbox"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GLB_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GLB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GLB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GLB_DIST"
+		optimize "On"
